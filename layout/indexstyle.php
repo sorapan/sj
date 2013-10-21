2630149fdc0c86faa1a2608544dc26149ec0e490
sorapan <t.sorapan@gmail.com>
2013-10-21 14:16:09 +0700

    start to make layout

---
+++ layout/indexstyle.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello</title>
+    <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
+
+    <link rel="stylesheet" href="../css/style1.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <script src="../js/jquery/jquery.js"></script>
+    <script src="../js/plugin/Public.js"></script>
+    <script src="../js/index_page/PageController.js"></script>
+    <script src="../js/index_page/Content.js"></script>
+    <script src="../js/index_page/Loadmore.js"></script>
+
+
+</head>
+<body>
+
+<div id="header">
+    <div id="sign"><img src="../img/logo.jpg"></div>
+    <h2>สงขลาเจริญการช่าง - Insurance System</h2>
+</div>
+
+<div id="blogtop_menu">
+    <div id="top_menu">
+        <button>Sample</button>
+        <button>Sample</button>
+    </div>
+</div>
+
+
+<?php  ?>
+
+</body>
+</html>
