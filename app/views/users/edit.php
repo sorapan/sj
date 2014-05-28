c1260ce341117f6c60107b3c29493f2962882a83
Sorapan <t.sorapan@gmail.com>
2014-05-28 19:24:19 +0700

    V. alpha 0.4.10 - can't edit username when edit user profile

--- app/views/users/edit.php
+++ app/views/users/edit.php
@@ -3,7 +3,7 @@
 <span style="font-size: 20px">แก้ไขข้อมูลของ : <?php echo $this->target['username']; ?></span><br><br>
 <form method="POST" action="<?php echo URL?>users/editsave/<?php echo $this->target['id'];?>">
 
-    <label for="username">ชื่อผู้ใช้</label><input type="text" name="username" id="username" value="<?php echo $this->target['username']; ?>"><br>
+    <label for="username">ชื่อผู้ใช้</label><input readonly type="text" name="username" id="username" value="<?php echo $this->target['username']; ?>"><br>
     <label for="password">รหัสผ่าน</label><input type="text" name="password" id="password" value="<?php echo $this->target['password']; ?>"><br>
     <label for="class">ระดับ</label><select id="class" name="class" >
         <option value="user"  <?php if($this->target['class'] == 'user') echo "selected"; ?>>User</option>
